ea53d33e02b51d9beadbaad0994f5807eb5a904d
lujanfernaud <lujanfernaud@gmail.com>
2017-07-28 11:55:58 +0100

    Move logic to lib/caesar_cipher.rb

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,30 @@
+def encrypt(message, shift_factor)
+  lowercase       = [*(65..90)]
+  uppercase       = [*(97..122)]
+  character_bytes = [lowercase, uppercase]
+  new_message     = ""
+
+  message.each_byte do |character|
+    character_array = build_character_array(character_bytes, character)
+    new_character = find_new_character(character_array, character, shift_factor)
+    new_message << new_character.chr
+  end
+
+  new_message
+end
+
+# Select only the sub array that contains 'character' to keep the same case.
+def build_character_array(character_bytes, character)
+  character_bytes
+    .select { |sub_array| sub_array.include?(character) }
+    .flatten
+end
+
+def find_new_character(character_array, character, shift_factor)
+  # If the array is empty then 'character' is not a letter.
+  return character if character_array.empty?
+
+  character_position      = character_array.index(character)
+  character_array_rotated = character_array.rotate(shift_factor)
+  character_array_rotated[character_position]
+end
